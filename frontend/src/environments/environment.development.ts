5b2f608044beaad8c58292233bff932a72eda009
Mahmoud Hamra <Mahmoud.hmra.dev@gmal.com>
2025-12-14 18:13:59 +0300

    small fix

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://api.clindoctor.net/api',
+  baseurl: 'https://hospital.clindoctor.net',
   hopePaymentUrl: 'https://payment.clindoctor.net/api/makeHash',
+  paymenturl: 'https://payment.clindoctor.net',
   hopePaymentProjectId: '4d2ff084-1afd-4c6a-b2e7-6e8de34bd6c8',
   hopePaymentProjectName: 'clindoctor_test',
   hopePaymentCurrency: 'USD',
   websocketUrl: 'ws://localhost:6001',
   websocketPort: 6001,
-  onlineMeetingApiUrl: 'https://call.clindoctor.net/api/v1/meeting'
-
+  onlineMeetingApiUrl: 'https://call.clindoctor.net'
 };
