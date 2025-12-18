ed30b27e2f2432b658e3675a0f3f0664e724ba99
Mahmoud Hamra <Mahmoud.hmra.dev@gmal.com>
2025-12-18 15:32:54 +0200

    aaaaaad

--- frontend/src/app/feature-module/common/common-components.module.ts
+++ frontend/src/app/feature-module/common/common-components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, FooterComponent, RouterModule],
 })
 export class FeatureCommonComponentsModule {}
